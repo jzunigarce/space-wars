b179a98cf33d433129770c1410d002cee3c61b42
Jesús Antonio Zúñiga Arce <jzunigarce@gmail.com>
2018-03-22 18:23:37 -0700

    remove files

--- sketch.js
+++ sketch.js
@@ -1,34 +1,9 @@
-const delay = 500
-let spaceImage
-let spaceShipImage
-let spaceSprite
-let spaceShipSprite
-let time
-
-function preload() {
-	spaceImage = loadImage('images/sky.png')
-	spaceShipImage = loadImage('images/spaceship.gif')
-}
-
 function setup() {
     createCanvas(windowWidth, windowHeight)
-	time = millis()
-    loadSprites()
 }
 
 function draw() {
-    background('#fff')
-    spaceSprites.draw()
-	spaceShipSprite.draw()
-	if(delay + time < millis()) {
-		spaceShipSprite.next()
-		time = millis()
-	}
 }
 
 const loadSprites = function() {
-    spaceSprites = new SpaceSprites(spaceImage)
-    spaceSprites.create()
-	spaceShipSprite = new SpaceCraftSprite(spaceShipImage)
-	spaceShipSprite.create()
 }
